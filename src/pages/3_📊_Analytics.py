f87d88593cd8048ffe90428c71f88b82162175be
marianoog <contacto@marianoog.com>
2024-12-26 00:18:01 +0100

    rename repositories and add simple things to the other pages

---
+++ src/pages/3_📊_Analytics.py
@@ -0,0 +1,7 @@
+from streamlit import st
+
+def main():
+    st.title("📊 Analytics")
+
+if __name__ == "__main__":
+    main()
---
+++ src/pages/4_⚙️_Settings.py
@@ -0,0 +1,7 @@
+from streamlit import st
+
+def main():
+    st.title("⚙️ Settings")
+
+if __name__ == "__main__":
+    main()
